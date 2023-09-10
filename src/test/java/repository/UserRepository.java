e912a4773562530ee79a0f695746cb7262d9ff5e
samyarjahroodi <samyarghasemjahroodi@gmail.com>
2023-09-10 12:53:49 +0330

    prepared statement added

--- src/test/java/repository/UserRepository.java
+++ src/test/java/repository/UserRepository.java
@@ -1,5 +1,28 @@
 package repository;
 
+import connection.JdbcConncetion;
+import model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserRepository {
+    JdbcConncetion jdbcConncetion = new JdbcConncetion();
+    Connection connection = jdbcConncetion.getConnection();
+
+    public UserRepository() throws SQLException {
+    }
+
+    public int save(User user) throws SQLException {
+        String add = "INSERT INTO users(firstName,lastName,password,userName)VALUES(?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(add);
+        preparedStatement.setString(1, user.getFirstName());
+        preparedStatement.setString(2, user.getLastName());
+        preparedStatement.setString(3, user.getPassword());
+        preparedStatement.setString(4, user.getUserName());
+        int result=preparedStatement.executeUpdate();
+        return result;
+    }
 
 }
