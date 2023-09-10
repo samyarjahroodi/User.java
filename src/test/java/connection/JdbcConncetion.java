12a91316295a75f029e7d5a654669234ceb81fc4
samyarjahroodi <samyarghasemjahroodi@gmail.com>
2023-09-10 12:54:30 +0330

    some changes added in order to insert data into the table

--- src/test/java/connection/JdbcConncetion.java
+++ src/test/java/connection/JdbcConncetion.java
@@ -5,9 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcConncetion {
-    Connection connection = DriverManager.getConnection(
+    private Connection connection = DriverManager.getConnection(
             "jdbc:postgresql://localhost:5432/postgres", "postgres", "1381Iran");
 
     public JdbcConncetion() throws SQLException {
+    }
+
+    public Connection getConnection() {
+        return connection;
     }
 }
